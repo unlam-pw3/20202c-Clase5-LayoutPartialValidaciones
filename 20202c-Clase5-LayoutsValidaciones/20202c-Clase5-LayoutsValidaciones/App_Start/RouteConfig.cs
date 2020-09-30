86952a399d6b75447b76225cfa763871e9ef4d33
psanchez <psanchez@plus54.com>
2020-09-30 19:40:50 -0300

    agregando layout con RenderBody y RenderSection

--- 20202c-Clase5-LayoutsValidaciones/20202c-Clase5-LayoutsValidaciones/App_Start/RouteConfig.cs
+++ 20202c-Clase5-LayoutsValidaciones/20202c-Clase5-LayoutsValidaciones/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace _20202c_Clase5_LayoutsValidaciones
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Bienvenido", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ 20202c-Clase5-LayoutsValidaciones/20202c-Clase5-LayoutsValidaciones/Controllers/BienvenidoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _20202c_Clase5_LayoutsValidaciones.Controllers
+{
+    public class BienvenidoController : Controller
+    {
+        // GET: Bienvenido
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult VistaConRegiones()
+        {
+            return View();
+        }
+    }
+}
