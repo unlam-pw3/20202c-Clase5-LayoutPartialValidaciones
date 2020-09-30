30f9dfe6f403ed0343de8d3ab9fec05dad6e7ea7
psanchez <psanchez@plus54.com>
2020-09-30 20:36:06 -0300

    agregando alta usuario sin validaciones

---
+++ 20202c-Clase5-LayoutsValidaciones/20202c-Clase5-LayoutsValidaciones/Controllers/UsuariosController.cs
@@ -0,0 +1,46 @@
+﻿using _20202c_Clase5_LayoutsValidaciones.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _20202c_Clase5_LayoutsValidaciones.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public static List<Usuario> ListaUsuarios { get; set; }
+        // GET: Usuarios
+        public UsuariosController()
+        {
+            if (ListaUsuarios == null)
+            {
+                ListaUsuarios = new List<Usuario>();
+            }
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Alta()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Alta(Usuario usuario)
+        {
+            ListaUsuarios.Add(usuario);
+            //required
+            //stringlength
+            //rango
+            //compare
+            //regularexpression
+            //customvalidation
+            //emailaddress
+            return View();
+        }
+    }
+}
--- 20202c-Clase5-LayoutsValidaciones/20202c-Clase5-LayoutsValidaciones/Models/Usuario.cs
+++ 20202c-Clase5-LayoutsValidaciones/20202c-Clase5-LayoutsValidaciones/Models/Usuario.cs
@@ -10,5 +10,9 @@ namespace _20202c_Clase5_LayoutsValidaciones.Models
         public int IdUsuario { get; set; }
         public string AvatarUrl { get; set; }
         public string NombreUsuario { get; set; }
+        public string Email { get; set; }
+        public string Contrasenia { get; set; }
+        public int Edad { get; set; }
+        public string Biografia { get; set; }
     }
 }
